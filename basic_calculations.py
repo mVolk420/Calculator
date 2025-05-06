234ef27bd83fabc2ad76db37f18e2b36090f04e6
mvolk420 <mvolk587@gmail.com>
2025-05-06 11:26:54 +0200

    implemented operations dictionary

--- basic_calculations.py
+++ basic_calculations.py
@@ -10,12 +10,9 @@ def mult (number_1 : float, number_2 : float) -> float :
 def div (number_1 : float, number_2 : float) -> float :
     return number_1 / number_2 
 
-def calculate (number_1 : float, number_2: float, operator : str):
-    if operator == "+":
-        return add(number_1, number_2)
-    elif operator == "-":
-        return sub(number_1, number_2)
-    elif operator == "*":
-        return mult(number_1, number_2)
-    
-    return div(number_1, number_2)
+operations = {
+    "+" : add,
+    "-" : sub,
+    "*" : mult,
+    "/" : div,
+}
--- main.py
+++ main.py
@@ -9,6 +9,6 @@ while True:
         number_1 = result
     number_2 = float(input("Second Number: "))
     operator = input("pick an operator: ")
-    result = basic_calculations.calculate(number_1, number_2, operator)
+    result = basic_calculations.operations[operator](number_1, number_2)
     print(str(number_1) + " " + operator + " " + str(number_2) + " = " + str(result))
     con = input("type y to continue calculating with " + str(result)+ "or n for another calculation ")
